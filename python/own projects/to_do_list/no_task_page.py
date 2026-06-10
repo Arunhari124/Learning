6b88135794c4e1f576fdeeb05708d9c80fa70a0d
Arunhari124 <arunsarunsarun058@gmail.com>
2026-06-10 19:47:01 +0530

    completed no_task_page ui

--- python/own projects/to_do_list/no_task_page.py
+++ python/own projects/to_do_list/no_task_page.py
@@ -1,12 +1,13 @@
 def no_task():
+    print("________________________________")
     print("=         | Tasks    |       🔎\n|         |all lists |        |")
     print("|                             |")
     print("|                             |\n|                             |\n|             ✅              |")
-    print("           No Tasks")
+    print("|           No Tasks          |")
 
 
-    print("\n\n                                |+|")
+    print("|                             |\n|                          |+||")
+    print("|_____________________________|")
 
 
 
-no_task()
