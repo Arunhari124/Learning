abd824ed5e4150ec444cbc6be365e694d1c3caa2
Arunhari124 <arunsarunsarun058@gmail.com>
2026-06-10 19:42:55 +0530

    changing ui of no_task_page

--- python/own projects/to_do_list/no_task_page.py
+++ python/own projects/to_do_list/no_task_page.py
@@ -1,9 +1,12 @@
 def no_task():
-    print("=          | Tasks   |          🔎\n           |all lists|       ")
-
-
-    print("\n\n             ✅")
+    print("=         | Tasks    |       🔎\n|         |all lists |        |")
+    print("|                             |")
+    print("|                             |\n|                             |\n|             ✅              |")
     print("           No Tasks")
 
 
     print("\n\n                                |+|")
+
+
+
+no_task()
