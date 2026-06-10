0322ac6768e066729f70e672d79990c0093d2bd9
Arunhari124 <arunsarunsarun058@gmail.com>
2026-06-10 20:09:36 +0530

    new_task_page ui changed

--- python/own projects/to_do_list/new_task_page.py
+++ python/own projects/to_do_list/new_task_page.py
@@ -1,11 +1,15 @@
 
 def output():
-        print("⬅️  New Task")
-        print()
-        print("  What is to be done?\n")
-        task_input=input("  Enter Task Here:")
-        print()
-        time_inpt=input("  Enter time in HH:MM:SS format:")
+        print("__________________________")
+        print("⬅️         New Task        |")
+        print("|                         |")
+        print("| *WHAT IS TO BE DONE?    |\n|                         |")
+        task_input=input("|Enter Task Here:")
+        print("|                         |")
+        time_inpt=input("|Enter time in HH:MM:SS format:")
+        print("__________________________")
         return task_input,time_inpt
             
         
+
+
