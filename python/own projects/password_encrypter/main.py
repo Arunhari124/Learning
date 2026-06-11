639e872840d9e34c3f47ba08a8fa08c93feaa06b
Arunhari124 <arunsarunsarun058@gmail.com>
2026-06-11 19:26:18 +0530

    password encrypter completed

--- python/own projects/password_encrypter/main.py
+++ python/own projects/password_encrypter/main.py
@@ -6,4 +6,4 @@ from encrypting import password_encrypter
 if __name__=="__main__":
     password=input("Enter password to encrypt:")
     encrypted=password_encrypter(password)
-    print(f"Encrypted password of your password is\n \n                              {encrypted}\n\n")
+    print(f"Encrypted password of your password is\n                               copy👇\n                       ====   |{encrypted}|       =====\n\n")
