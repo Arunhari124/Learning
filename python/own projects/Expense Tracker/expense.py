c2054dc47730e070670bc7a86b549450940d4fd2
Arunhari124 <arunsarunsarun058@gmail.com>
2026-06-14 18:58:01 +0530

    file save

--- python/own projects/Expense Tracker/expense.py
+++ python/own projects/Expense Tracker/expense.py
@@ -1,19 +1,21 @@
 
-
+import json
 def expense():
     amount=float(input("ENter your monthly income: "))
-    expene={}
+    
     is_running=True 
     while is_running:
     
         exp=input("Enter the item:/S:")
+          
     
         if exp.upper()=="S":
             is_running=False
         else:
             amnt=float(input("ENTE THE Amont:"))
             expene[exp]=amnt
-
+            with open("expense.json", "w") as file:
+                    json.dump(expene, file, indent=4)
 
     print(expene)  
     s=sum(expene.values())
@@ -25,5 +27,13 @@ def expense():
         print("WON")
     print(s)
     
+
+
+try:
+    with open("expense.json", "r") as file:
+            expene = json.load(file)   
+except Exception:
+    expene={}
     
+        
 expense()
