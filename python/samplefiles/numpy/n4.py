7e0d649d0e1641116446e91331577c98ad33c9c0
Arunhari124 <arunsarunsarun058@gmail.com>
2026-06-16 21:14:09 +0530

    radius of circle in numpy

--- python/samplefiles/numpy/n4.py
+++ python/samplefiles/numpy/n4.py
@@ -1,13 +1,17 @@
 import numpy as np
 
-array=np.array([1.01,2.5,3.99])
+#array=np.array([1.01,2.5,3.99])
 
 #print(array-2)
 #print(array+2)
 #print(array/2)
 #print(array*2)
 #print(array**2)
-print(np.sqrt(array))
-print(np.floor(array))
-print(np.ceil(array))
-print(np.pi)
+#print(np.sqrt(array))
+#print(np.floor(array))
+#print(np.ceil(array))
+#print(np.pi)
+
+
+radii=np.array([1,2,3])
+print(np.pi * radii ** 2)
