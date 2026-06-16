280cbaa106976a43ea0fcf2ed04c2b86bdcbb505
Arunhari124 <arunsarunsarun058@gmail.com>
2026-06-16 22:45:33 +0530

    changed random generator method to numpy random genrator

--- python/own projects/number_guessing_game/random_numgen.py
+++ python/own projects/number_guessing_game/random_numgen.py
@@ -1,11 +1,18 @@
-import random
+
+import numpy as np
 def rand(num1,num2):
-    x=random.randint(num1,num2)
+    rng=np.random.default_rng()
     print(f"values will be btw{num1} and {num2}")
-    return x
+    return rng.integers(num1,num2)
+
+    
+
 def defualt_rand():
-    x=random.randint(1,10)
+    rng=np.random.default_rng()
+
     print("values will be btw 1 and 10")
-    return x
+    return rng.integers(1,11)
+
+    
     
     
