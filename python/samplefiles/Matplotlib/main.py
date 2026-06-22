c3f44fb0e1ad02c3674a121855c6598250caeaf7
Arunhari124 <arunsarunsarun058@gmail.com>
2026-06-22 20:48:24 +0530

    plot customisation

--- python/samplefiles/Matplotlib/main.py
+++ python/samplefiles/Matplotlib/main.py
@@ -4,7 +4,18 @@ import numpy as np
 
 x=np.array([1,2,4,5,6])
 y=np.array([1,3,2,3,1])
+z=np.array([2,4,3,4,2])
+z1=np.array([3,5,4,5,3])
+dict1=dict(marker="o",markersize=10,markerfacecolor="green",
+         markeredgecolor="red",linestyle="dotted",
+         color="red")
 
-plt.plot(x,y)
-
+plt.title("Class size",fontsize=20,family="Arial",fontweight="bold",color="blue")
+plt.xlabel("Year",fontsize=20,family="Arial",fontweight="bold",color="yellow")
+plt.ylabel("food",fontsize=20,family="Arial",fontweight="bold",color="yellow")
+plt.plot(x,y, **dict1)
+plt.plot(x,z,**dict1)
+plt.plot(x,z1,**dict1)
+#plt.xticks(x)
+#plt.tick_params(axis="both",colors="black")
 plt.show()
